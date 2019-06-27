1fd4321ca6a02c6cdd6b86edd35e09816e1049ea
Imabsn.com <ljqft0536@foxmail.com>
2019-06-27 18:49:41 +0800

    样式微调

--- footer.php
+++ footer.php
@@ -15,21 +15,15 @@
                 <?= $this->options->ButtomText ?>
             <?php endif; ?>
             <?php if ($this->options->SiteTime): ?>
-                <p>Has been running for <a id="runtime_span"></a>.</p>
+                <p>已安稳运行<a id="runtime_span"></a>.</p>
                 <script type="text/javascript">function show_runtime() {
-                        window.setTimeout("show_runtime()", 1000);
                         X = new Date("<?=$this->options->SiteTime?>");
                         Y = new Date();
                         T = (Y.getTime() - X.getTime());
                         M = 24 * 60 * 60 * 1000;
                         a = T / M;
                         A = Math.floor(a);
-                        b = (a - A) * 24;
-                        B = Math.floor(b);
-                        c = (b - B) * 60;
-                        C = Math.floor((b - B) * 60);
-                        D = Math.floor((c - C) * 60);
-                        runtime_span.innerHTML = "" + A + " Day " + B + " Hour " + C + " Min " + D + " Sec"
+                        runtime_span.innerHTML = "" + A + "天"
                     }
 
                     show_runtime();</script>
